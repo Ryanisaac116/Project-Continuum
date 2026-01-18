df883ee26827226cedb47b71068a2c8bbb39cf94
Ryanisaac116 <ryanisaac116@gmail.com>
2026-01-18 20:57:41 +0530

    remove @profile

--- src/main/java/com/Project/Continuum/controller/AuthController.java
+++ src/main/java/com/Project/Continuum/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
-@Profile({ "prod", "dev" })
 public class AuthController {
 
     private final GoogleAuthService googleAuthService;
